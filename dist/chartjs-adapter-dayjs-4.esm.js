40e61c4bee4c4b65ab30efba1c970532cc3ba4ed
Anton Gorpinchenko <Anton.Gorpinchenko@conjointly.com>
2024-10-04 16:32:29 +0400

    test

--- dist/chartjs-adapter-dayjs-4.esm.js
+++ dist/chartjs-adapter-dayjs-4.esm.js
@@ -1,34 +1,17 @@
 import { _adapters } from 'chart-js-v4';
-
-import dayjs, { QUnitType } from 'dayjs';
-
-import type { TimeUnit } from 'chart-js-v4';
-
-// Needed to handle the custom parsing
+import dayjs from 'dayjs';
 import CustomParseFormat from 'dayjs/plugin/customParseFormat.js';
-
-// Needed to handle quarter format
 import AdvancedFormat from 'dayjs/plugin/advancedFormat.js';
-
-// Needed to handle adding/subtracting quarter
 import QuarterOfYear from 'dayjs/plugin/quarterOfYear.js';
-
-// Needed to handle localization format
 import LocalizedFormat from 'dayjs/plugin/localizedFormat.js';
-
 import isoWeek from 'dayjs/plugin/isoWeek.js';
 
 dayjs.extend(AdvancedFormat);
-
 dayjs.extend(QuarterOfYear);
-
 dayjs.extend(LocalizedFormat);
-
 dayjs.extend(CustomParseFormat);
-
 dayjs.extend(isoWeek);
-
-const FORMATS = {
+var FORMATS = {
   datetime: 'MMM D, YYYY, h:mm:ss a',
   millisecond: 'h:mm:ss.SSS a',
   second: 'h:mm:ss a',
@@ -38,19 +21,18 @@ const FORMATS = {
   week: 'll',
   month: 'MMM YYYY',
   quarter: '[Q]Q - YYYY',
-  year: 'YYYY',
+  year: 'YYYY'
 };
-
 _adapters._date.override({
   //_id: 'dayjs', //DEBUG,
-  formats: () => FORMATS,
-  parse: function (value: any, format?: TimeUnit) {
-    const valueType = typeof value;
-
+  formats: function formats() {
+    return FORMATS;
+  },
+  parse: function parse(value, format) {
+    var valueType = typeof value;
     if (value === null || valueType === 'undefined') {
       return null;
     }
-
     if (valueType === 'string' && typeof format === 'string') {
       return dayjs(value, format).isValid() ? dayjs(value, format).valueOf() : null;
     } else if (!(value instanceof dayjs)) {
@@ -58,29 +40,26 @@ _adapters._date.override({
     }
     return null;
   },
-  format: function (time: any, format: TimeUnit): string {
-    return dayjs(time).format(format);
+  format: function format(time, _format) {
+    return dayjs(time).format(_format);
   },
-  add: function (time: any, amount: number, unit: QUnitType & TimeUnit) {
+  add: function add(time, amount, unit) {
     return dayjs(time).add(amount, unit).valueOf();
   },
-  diff: function (max: any, min: any, unit: TimeUnit) {
+  diff: function diff(max, min, unit) {
     return dayjs(max).diff(dayjs(min), unit);
   },
-  startOf: function (time: any, unit: (TimeUnit & QUnitType) | 'isoWeek', weekday?: number) {
+  startOf: function startOf(time, unit, weekday) {
     if (unit === 'isoWeek') {
       // Ensure that weekday has a valid format
       //const formattedWeekday
-
-      const validatedWeekday: number =
-        typeof weekday === 'number' && weekday > 0 && weekday < 7 ? weekday : 1;
-
+      var validatedWeekday = typeof weekday === 'number' && weekday > 0 && weekday < 7 ? weekday : 1;
       return dayjs(time).isoWeekday(validatedWeekday).startOf('day').valueOf();
     }
-
     return dayjs(time).startOf(unit).valueOf();
   },
-  endOf: function (time: any, unit: TimeUnit & QUnitType) {
+  endOf: function endOf(time, unit) {
     return dayjs(time).endOf(unit).valueOf();
-  },
+  }
 });
+//# sourceMappingURL=chartjs-adapter-dayjs-4.esm.js.map
